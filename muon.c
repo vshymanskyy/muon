821e1d5fbef65c023bc0c834d20e3a387074a15d
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-02-28 15:51:51 +0200

    Update muon.c

--- muon.c
+++ muon.c
@@ -10,24 +10,24 @@
 
 // Defines for compile-time constructiion
 #define STRINGIFY(x) #x
-#define uEND         "\0"
-#define uVAL(v)      uSTR(STRINGIFY(v)) // Utility to stringify constants
-#define uSTR(s)      s uEND             // Every value is null-terminated
-#define uKV(k,v)     uSTR(k) v          // Key-value pairs are just pairs
+#define uEND		 "\0"
+#define uVAL(v)	     uSTR(STRINGIFY(v)) // Utility to stringify constants
+#define uSTR(s)	     s uEND			    // Every value is null-terminated
+#define uKV(k,v)	 uSTR(k) v		    // Key-value pairs are just pairs
 #define uLIST(items) "\01" items "\00"	// LIST: 1 and 0 are like [ and ]
 #define uDICT(items) "\02" items "\00"  // DICT: 2 and 0 are like { and }
 #define uMETA(items) "\03" items "\00"  // ATTR: dict of attributes
-#define uBLOB(s, b)  "\04" s b          // Binary is prefixed with length
+#define uBLOB(s, b)  "\04" s b		    // Binary is prefixed with length
 
-size_t muon_length(const char* beg, const char* end)
+size_t muon_length(const char* beg, size_t len)
 {
 	unsigned depth = 0;
-	for (const char *ptr = beg; ptr < end; ) {
+	for (const char *ptr = beg, *end = beg+len; ptr < end; ) {
 		switch(*ptr) {
 		case MUON_BLOB_BEG: {
-			const long len = atol(++ptr);
+			const long l = atol(++ptr);
 			ptr += strlen(ptr) + 1;
-			ptr += len;
+			ptr += l;
 		}	break;
 		case MUON_LIST_BEG:
 		case MUON_DICT_BEG:
@@ -45,7 +45,7 @@ size_t muon_length(const char* beg, const char* end)
 		if (!depth)
 			return ptr-beg;
 	}
-	return end-beg;
+	return len;
 }
 
 void muon_pretty_print(const char* ptr)
@@ -92,27 +92,31 @@ int main()
 {
 	// Compile-time construction
 	const char uObj[] =
-	    uDICT(
-	    	uMETA(
-	    		uKV("encoding", uSTR("utf8"))
-	    	)
-	        uKV("abc", uVAL(123))
-	        uKV("def", uSTR("456"))
-	        uKV("map", uDICT(
-	            uKV("k1", uSTR("v1"))
-	            uKV("k2", uSTR("v2"))
+		uDICT(
+			uMETA(
+				uKV("encoding", uSTR("utf8"))
+			)
+			uKV("abc", uVAL(123))
+			uKV("def", uSTR("456"))
+			uKV("map", uDICT(
+				uKV("k1", uSTR("v1"))
+				uKV("k2", uSTR("v2"))
 				uKV("k3", uLIST(
 					"i0" uEND "i1" uEND "i2" uEND "i3" uEND
 				))
-	        ))
-	        uKV("list", uLIST(
-	        		"i0" uEND "i1" uEND "i2" uEND "i3" uEND
-	        ))
+			))
+			uKV("list", uLIST(
+					"i0" uEND "i1" uEND "i2" uEND "i3" uEND
+			))
 			uKV("bin", uBLOB(uVAL(5), "\1\2\3\4\5"))
 			uKV("last", uSTR("val"))
-	    );
+		);
 	
-  muon_pretty_print(uObj);
+	// For this object, size is 109
+	printf("uObj sizeof: %lu\n", sizeof(uObj)-1);
+	printf("uObj length: %lu\n", muon_length(uObj, sizeof(uObj)));
+	
+	muon_pretty_print(uObj);
   
-  return 0;
+	return 0;
 }
